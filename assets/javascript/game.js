5f7a39aae8a3e8997ced7c71f9fd5001566c0252
Nicole LaSusa <nicolelasusa@gmail.com>
2019-06-09 18:09:48 -0400

    initialcommit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,30 +2,113 @@
 $(document).ready(function(){ 
 
 // Variables 
-
+var userTotal = 0; 
 var wins= 0;
 var losses =0; 
+var newUserTotal="";
 var totalScore =0; 
 
 // Magic number from ranges 30-130
-    var Random=Math.floor(Math.random()*100+30);
-    console.log (Random)
+var randomNum = Math.floor(Math.random()*100+30);
+    console.log (randomNum)
 
 // Append magic # to id in HTML 
-$("#magicNumber").text(Random);
+$("#magicNumber").text(randomNum);
 
-// Random numbers from 1-21 for each crystal 
-var crys1 = Math.floor(Math.random()*20+1);
-var crys2 = Math.floor(Math.random()*20+1);
-var crys3 = Math.floor(Math.random()*20+1);
-var crys4 = Math.floor(Math.random()*20+1);
+//Apend wins and lossees to id in HTML 
+$("#numberWins").text(wins);
+$("#numberLosses").text(losses);
+
+// Random numbers from 1-19 for each crystal 
+var crys1 = Math.floor(Math.random()*18+1);
+var crys2 = Math.floor(Math.random()*18+1);
+var crys3 = Math.floor(Math.random()*18+1);
+var crys4 = Math.floor(Math.random()*18+1);
     console.log (crys1)
     console.log (crys2)
     console.log (crys3)
     console.log (crys4)
-})
 
-$("#one").on("click", function() {
-    var crys1;
-    console.log (crys1);
+function resetGame (){
+    randomNum = Math.floor(Math.random()*100+30);
+    console.log (randomNum) 
+    $("#magicNumber").text(randomNum);
+    crys1 = Math.floor(Math.random()*18+1);
+    crys2 = Math.floor(Math.random()*18+1);
+    crys3 = Math.floor(Math.random()*18+1);
+    crys4 = Math.floor(Math.random()*18+1);
+    userTotal= 0; 
+    $("#totalScore").text(userTotal);
+}  
+
+// alert and wins added for user 
+function winner () {
+    alert ("Awesome, you got it!");
+    wins++;
+    $("numberWins").text(wins);
+    resetGame();
+}
+
+// alert and losses added for user 
+function loser (){
+    alert ("You Lose! Better luck next time!");
+    losses++;
+    $("numberLosses").text(losses);
+    resetGame();
+}
+
+// click functions for crys1, crys2, crys3 and crys 4
+$("#img1").on("click", function(){
+    userTotal = userTotal + crys1;
+    console.log("NewUserTotal=" + userTotal);
+    $("#totalScore").text(userTotal);
+
+// if else conditions 
+    if (userTotal == randomNum){
+        winner();
+    }
+    else if (userTotal > randomNum){
+        loser();
+    }
+})
+$("#img2").on("click", function(){
+    userTotal = userTotal + crys2;
+    console.log("NewUserTotal=" + userTotal);
+    $("#totalScore").text(userTotal);
+
+// if else conditions
+    if (userTotal == randomNum){
+        winner();
+    }
+    else if (userTotal > randomNum){
+        loser();
+    }
+})
+$("#img3").on("click", function(){
+    userTotal = userTotal + crys3;
+    console.log("NewUserTotal=" + userTotal);
+    $("#totalScore").text(userTotal);
+
+// if else conditions
+    if (userTotal == randomNum){
+        winner();
+    }
+    else if (userTotal > randomNum){
+        loser();
+    }
+$("#img4").on("click", function(){
+    userTotal = userTotal + crys4;
+    console.log("NewUserTotal=" + userTotal);
+    $("#totalScore").text(userTotal);
+    
+    // if else conditions
+    if (userTotal == randomNum){
+        winner();
+    }
+    else if (userTotal > randomNum){
+        loser();
+    }
+    })
 });
+});
+
